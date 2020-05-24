fdc2075f83d9fbd33afad9769f88f9923f372a7f
Florian Soller <flsoller@gmail.com>
2020-05-24 16:38:26 +0200

    Add render function to display books in table.

--- script.js
+++ script.js
@@ -19,8 +19,33 @@ book.prototype.addBook =  function() {
     });
 }
 
-// let book1 = new book('AwesomeTitle', 'MagicAuthor', 534, false);
+//Dummy books
+let book1 = new book('AwesomeTitle', 'MagicAuthor', 534, 'Yes').addBook();
+let book2 = new book('MegaTitle', 'MegaAuthor', 123, 'No').addBook();
 
-// book1.addBook();
-// console.log(book1)
-// console.table(myLibrary);
+
+// Append Library to table
+
+function render() {
+const libraryTable = document.getElementById('libraryTable');
+
+
+    //New Row for each book object in the library array
+    for (let i = 0 ; i < myLibrary.length ; i++) {
+        let newRow = libraryTable.insertRow(i+1);
+
+        //Create number of cells equivalent to book properties for each row
+        for (c = 0; c < book.length; c++) {
+        let newCell = newRow.insertCell(newRow);
+        }
+
+        //Assigns each book property value to it's corresponding cell
+        newRow.children[0].innerHTML = myLibrary[i].title;
+        newRow.children[1].innerHTML = myLibrary[i].author;
+        newRow.children[2].innerHTML = myLibrary[i].pages;
+        newRow.children[3].innerHTML = myLibrary[i].read;
+
+    }
+}
+
+render();
